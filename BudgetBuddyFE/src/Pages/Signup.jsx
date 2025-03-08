efded95c0bc9e3e2ef1c0fb8f423d41431fb1000
nightburnn <nightburn2003@gmail.com>
2025-03-08 17:25:59 +0100

    fixed deploy error

--- BudgetBuddyFE/src/Pages/Signup.jsx
+++ BudgetBuddyFE/src/Pages/Signup.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
 import "./Signup.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { useNavigat, Link } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
